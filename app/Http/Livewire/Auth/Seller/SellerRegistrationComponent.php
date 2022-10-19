40c41672b8314d8aaf3bf6b9e828d8c2ad52f606
AhmedSHG97 <ahmed2012ramadan@gmail.com>
2022-10-19 17:22:10 +0200

    adding referral module and referral_code to sellers data in dashboard table

--- app/Http/Livewire/Auth/Seller/SellerRegistrationComponent.php
+++ app/Http/Livewire/Auth/Seller/SellerRegistrationComponent.php
@@ -84,6 +84,7 @@ class SellerRegistrationComponent extends Component
             $seller->name = $this->first_name.' '.$this->last_name;
             $seller->email = $this->email;
             $seller->phone = $this->phone;
+            $seller->referral_code = $this->referral_code;
             $seller->password = user()->password;
             $seller->save();
 
@@ -145,8 +146,9 @@ class SellerRegistrationComponent extends Component
             $seller->email = $this->email;
             $seller->phone = $this->phone;
             $seller->password = Hash::make($this->password);
+            $seller->referral_code = $this->referral_code;
             $seller->save();
-            
+
             $wallet = new SellerWallet();
             $wallet->seller_id = $seller->id;
             $wallet->save();
